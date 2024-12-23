889c652bb932d1781fcd14e22784fe60d73e9e0b
Atharva Kulkarni <atharvk2109@gmail.com>
2024-12-23 17:32:54 +0530

    Created NavBar for Brand Page

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <nav>
+      <div className="logo">
+        <img src="/images/brand_logo.png" alt="brand_logo" />
+      </div>
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ul>
+      <button>Login</button>
+    </nav>
+  );
+}
